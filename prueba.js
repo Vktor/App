dcc92980caf6997603ee5a21d7eab6c060afea9d
Victor <victor.carranz@hotmail.com>
2018-06-20 10:41:35 -0600

    se agregan librerias y servidor

---
+++ prueba.js
@@ -0,0 +1,10 @@
+dataSet = '[{"idMarca":1,"nombreMarca":"Dell","descripcion":"Empresa de tecnologia","email":"dell-corporatio@outlook.com","telefono":"2212323","website":null,"direccion":"USA"}]';
+// let   myRecords = JSON.parse($records.text());
+
+$(document).ready(function() {
+    $('#tabla').DataTable( {
+        "processing": true,
+        "serverSide": true,
+        "ajax": dataSet
+    } );
+} );
